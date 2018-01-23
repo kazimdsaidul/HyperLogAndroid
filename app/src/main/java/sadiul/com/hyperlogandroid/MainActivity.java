f1a5f92c5455b3b95d05544ac6b5d9f74d317cf0
Kazi Md. Saddul <kazimdsaidul@gmail.com>
2018-01-23 20:16:19 +0600

    initilization commit

---
+++ app/src/main/java/sadiul/com/hyperlogandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package sadiul.com.hyperlogandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
